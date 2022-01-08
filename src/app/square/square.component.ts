02ec60200b17483302bf2d2a10a0e524a69db005
vicente <vsolis199@DESKTOP-VS.localdomain>
2022-01-08 17:06:51 -0300

    game-functionality

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,20 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
+    <button>{{ value }}</button>
   `,
-  styles: [
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'
   ]
 })
 export class SquareComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input()
+  value: 'X' | 'O';
 
 }
